04fa2bfe508da7cee1f2752815c99859c678144d
ArfanTahnif <arfantahnif88@gmail.com>
2026-09-22 22:57:20 +0600

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,61 @@
+export const APP_CONFIG = {
+
+    name: "PTEC NoteBOT",
+
+    college:
+        "Pabna Textile Engineering College",
+
+    version:
+        "1.0.0",
+
+
+    theme:
+        "dark",
+
+
+    storageKey:
+        "ptec-notebot",
+
+
+
+    routes: [
+
+        "home",
+        "notes",
+        "search",
+        "texgpt",
+        "phonebook",
+        "games",
+        "profile"
+
+    ]
+
+};
+
+
+
+
+export const TEX_KB = {
+
+
+    cotton:
+        "Cotton is a natural fibre obtained from cotton plant. It is mainly composed of cellulose.",
+
+
+    warp:
+        "Warp yarns are longitudinal yarns prepared on loom before weaving.",
+
+
+    weft:
+        "Weft yarns are inserted horizontally through warp yarns during weaving.",
+
+
+    carding:
+        "Carding is a process that opens, cleans and aligns textile fibres.",
+
+
+    tex:
+        "Tex is yarn count system where weight in grams of 1000 meters yarn is measured."
+
+
+};
