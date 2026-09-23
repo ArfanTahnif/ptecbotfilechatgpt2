924ca529b672cae9816cc4c1b7857a8fd575c7c2
ArfanTahnif <arfantahnif88@gmail.com>
2026-09-23 19:58:13 +0600

    Create ui.js

---
+++ js/core/ui.js
@@ -0,0 +1,45 @@
+export function loading(){
+
+
+return `
+
+
+<div class="card">
+
+<h3>
+Loading...
+</h3>
+
+
+</div>
+
+
+`;
+
+}
+
+
+
+
+
+export function empty(message){
+
+
+return `
+
+
+<div class="card">
+
+
+<h3>
+${message}
+</h3>
+
+
+</div>
+
+
+`;
+
+
+}
