9442479167fd21ee562dee8a3b016a8a0e22c145
ArfanTahnif <arfantahnif88@gmail.com>
2026-09-23 19:42:43 +0600

    Create games.js

---
+++ js/features/games.js
@@ -0,0 +1,82 @@
+export function renderGames(){
+
+
+return `
+
+
+<section class="hero">
+
+
+<h1>
+🎮 Break Games
+</h1>
+
+
+<p>
+Relax your mind between study sessions
+</p>
+
+
+</section>
+
+
+
+
+<div class="grid">
+
+
+<div class="card"
+data-action="open-game"
+data-game="tictactoe">
+
+
+⭕❌
+
+<br>
+
+Tic Tac Toe
+
+
+</div>
+
+
+
+<div class="card"
+data-action="open-game"
+data-game="memory">
+
+
+🧵
+
+<br>
+
+Textile Memory
+
+
+</div>
+
+
+
+
+<div class="card"
+data-action="open-game"
+data-game="rps">
+
+
+✊
+
+<br>
+
+Rock Paper Scissors
+
+
+</div>
+
+
+
+</div>
+
+
+`;
+
+}
